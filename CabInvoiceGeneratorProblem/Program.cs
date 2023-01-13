35cbbf924778c54953fefef2060f2da496ff08c0
BashaVali <bashavali222@gmail.com>
2023-01-14 04:55:47 +0530

    Cab Invoice Generator

---
+++ CabInvoiceGeneratorProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Cab_Invoice_Generator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Cab Ride Problem Statement for testing");
+        }
+    }
+}
